eab0cdef828f69f4358a577ef4c9f9f9f1fff8ba
Romain Moreaux <romainmoreaux@Romains-MacBook-Pro.local>
2020-06-02 14:46:33 +0200

    refacto createBackgroundLayer, + Matrix

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,5 @@
-import Compositor from './Compositor.js'
 import { loadLevel } from './loaders.js'
 import { loadBackgroundSprites } from './sprites.js'
-import { createBackgroundLayer, createSpritesLayer } from './layers.js'
 import { createMario } from './entities.js'
 import Timer from './Timer.js'
 import Keyboard from './KeyboardState.js'
@@ -9,42 +7,29 @@ import Keyboard from './KeyboardState.js'
 const canvas = document.getElementById('screen')
 const context = canvas.getContext('2d')
 
-Promise.all([createMario(), loadBackgroundSprites(), loadLevel('1-1')]).then(
-  ([mario, backgroundSprites, level]) => {
-    const comp = new Compositor()
+Promise.all([createMario(), loadLevel('1-1')]).then(([mario, level]) => {
+  const gravity = 2000
+  mario.pos.set(64, 64)
 
-    const backgroundLayer = createBackgroundLayer(
-      level.backgrounds,
-      backgroundSprites
-    )
-    comp.layers.push(backgroundLayer)
+  level.entities.add(mario)
 
-    const spritesLayer = createSpritesLayer(mario)
-    comp.layers.push(spritesLayer)
-
-    const gravity = 2000
-    mario.pos.set(64, 180)
-    // mario.vel.set(200, -600)
-
-    const SPACE = 32
-    const input = new Keyboard()
-    input.addMaping(SPACE, (keyState) => {
-      console.log('Mario jumped', keyState)
-      if (keyState) {
-        mario.jump.start()
-      } else {
-        mario.jump.cancel()
-      }
-    })
-
-    input.listenTo(window)
-
-    const timer = new Timer(1 / 60)
-    timer.update = function update(deltaTime) {
-      mario.update(deltaTime)
-      comp.draw(context)
-      mario.vel.y += gravity * deltaTime
+  const SPACE = 32
+  const input = new Keyboard()
+  input.addMaping(SPACE, (keyState) => {
+    if (keyState) {
+      mario.jump.start()
+    } else {
+      mario.jump.cancel()
     }
-    timer.start()
+  })
+
+  input.listenTo(window)
+
+  const timer = new Timer(1 / 60)
+  timer.update = function update(deltaTime) {
+    level.update(deltaTime)
+    level.comp.draw(context)
+    mario.vel.y += gravity * deltaTime
   }
-)
+  timer.start()
+})
