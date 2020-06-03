40954c5157e82eeeb877c9cfaa4210654433d41d
Romain Moreaux <romainmoreaux@Romains-MacBook-Pro.local>
2020-06-03 12:08:48 +0200

    + Camera

--- public/js/layers.js
+++ public/js/layers.js
@@ -9,8 +9,9 @@ export function createBackgroundLayer(level, sprites) {
     sprites.drawTile(tile.name, context, x, y)
   })
 
-  return function drawBackgroundLayer(context) {
-    context.drawImage(buffer, 0, 0)
+  return function drawBackgroundLayer(context, camera) {
+    // context.drawImage(buffer, 0, 0)
+    context.drawImage(buffer, -camera.pos.x, -camera.pos.y)
   }
 }
 
@@ -22,32 +23,32 @@ export function createSpritesLayer(entities) {
   }
 }
 
-// export function createCollisionLayer(level) {
-//   const resolvedTiles = []
+export function createCollisionLayer(level) {
+  const resolvedTiles = []
 
-//   const tileResolver = level.tileCollider.tiles
-//   const tileSize = tileResolver.tileSize
+  const tileResolver = level.tileCollider.tiles
+  const tileSize = tileResolver.tileSize
 
-//   const getByIndexOriginal = tileResolver.getByIndex
-//   tileResolver.getByIndex = function getByIndexFake(x, y) {
-//     resolvedTiles.push({ x, y })
-//     return getByIndexOriginal.call(tileResolver, x, y)
-//   }
+  const getByIndexOriginal = tileResolver.getByIndex
+  tileResolver.getByIndex = function getByIndexFake(x, y) {
+    resolvedTiles.push({ x, y })
+    return getByIndexOriginal.call(tileResolver, x, y)
+  }
 
-//   return function drawCollision(context) {
-//     context.strokeStyle = 'blue'
-//     resolvedTiles.forEach(({ x, y }) => {
-//       context.beginPath()
-//       context.rect(x * tileSize, y * tileSize, tileSize, tileSize)
-//       context.stroke()
-//     })
+  return function drawCollision(context) {
+    context.strokeStyle = 'blue'
+    resolvedTiles.forEach(({ x, y }) => {
+      context.beginPath()
+      context.rect(x * tileSize, y * tileSize, tileSize, tileSize)
+      context.stroke()
+    })
 
-//     context.strokeStyle = 'red'
-//     level.entities.forEach((entity) => {
-//       context.beginPath()
-//       context.rect(entity.pos.x, entity.pos.y, entity.size.x, entity.size.y)
-//       context.stroke()
-//     })
-//     resolvedTiles.length = 0
-//   }
-// }
+    context.strokeStyle = 'red'
+    level.entities.forEach((entity) => {
+      context.beginPath()
+      context.rect(entity.pos.x, entity.pos.y, entity.size.x, entity.size.y)
+      context.stroke()
+    })
+    resolvedTiles.length = 0
+  }
+}
--- public/js/main.js
+++ public/js/main.js
@@ -2,11 +2,16 @@ import { loadLevel } from './loaders.js'
 import { createMario } from './entities.js'
 import Timer from './Timer.js'
 import { setupKeyboard } from './input.js'
+import { createCollisionLayer } from './layers.js'
+import Camera from './Camera.js'
 
 const canvas = document.getElementById('screen')
 const context = canvas.getContext('2d')
 
 Promise.all([createMario(), loadLevel('1-1')]).then(([mario, level]) => {
+  const camera = new Camera()
+
+  window.camera = camera
   mario.pos.set(64, 64)
 
   level.entities.add(mario)
@@ -15,21 +20,21 @@ Promise.all([createMario(), loadLevel('1-1')]).then(([mario, level]) => {
   input.listenTo(window)
 
   // For debugging purposes
-  // const collisionLayer = createCollisionLayer(level)
-  // level.comp.layers.push(collisionLayer)
-  // ;['mousedown', 'mousemove'].forEach((eventName) => {
-  //   canvas.addEventListener(eventName, (event) => {
-  //     if (event.buttons === 1) {
-  //       mario.vel.set(0, 0)
-  //       mario.pos.set(event.offsetX, event.offsetY)
-  //     }
-  //   })
-  // })
+  const collisionLayer = createCollisionLayer(level)
+  level.comp.layers.push(collisionLayer)
+  ;['mousedown', 'mousemove'].forEach((eventName) => {
+    canvas.addEventListener(eventName, (event) => {
+      if (event.buttons === 1) {
+        mario.vel.set(0, 0)
+        mario.pos.set(event.offsetX, event.offsetY)
+      }
+    })
+  })
 
   const timer = new Timer(1 / 60)
   timer.update = function update(deltaTime) {
     level.update(deltaTime)
-    level.comp.draw(context)
+    level.comp.draw(context, camera)
   }
   timer.start()
 })
