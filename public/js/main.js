521cf6a3519aa1c55904e4891e4fd1375beac747
Romain Moreaux <romainmoreaux@Romains-MacBook-Pro.local>
2020-06-03 14:51:46 +0200

    + setupMouseControl

--- public/js/main.js
+++ public/js/main.js
@@ -4,14 +4,17 @@ import Timer from './Timer.js'
 import { setupKeyboard } from './input.js'
 import { createCollisionLayer } from './layers.js'
 import Camera from './Camera.js'
+import { setupMouseControl } from './setupMouseControl.js'
 
 const canvas = document.getElementById('screen')
 const context = canvas.getContext('2d')
 
 Promise.all([createMario(), loadLevel('1-1')]).then(([mario, level]) => {
   const camera = new Camera()
-
   window.camera = camera
+  const collisionLayer = createCollisionLayer(level)
+  level.comp.layers.push(collisionLayer)
+
   mario.pos.set(64, 64)
 
   level.entities.add(mario)
@@ -20,16 +23,7 @@ Promise.all([createMario(), loadLevel('1-1')]).then(([mario, level]) => {
   input.listenTo(window)
 
   // For debugging purposes
-  const collisionLayer = createCollisionLayer(level)
-  level.comp.layers.push(collisionLayer)
-  ;['mousedown', 'mousemove'].forEach((eventName) => {
-    canvas.addEventListener(eventName, (event) => {
-      if (event.buttons === 1) {
-        mario.vel.set(0, 0)
-        mario.pos.set(event.offsetX, event.offsetY)
-      }
-    })
-  })
+  setupMouseControl(canvas, mario, camera)
 
   const timer = new Timer(1 / 60)
   timer.update = function update(deltaTime) {
